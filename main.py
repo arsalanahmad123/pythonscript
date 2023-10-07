228c31a08f66977f85939d1bc18dc3555601f888
arsalanahmad123 <arsalanahmad15092003@gmail.com>
2023-10-07 17:12:59 +0500

    again traversed

--- main.py
+++ main.py
@@ -34,36 +34,23 @@ def generate_chart_data(file_path, start_year, start_day, end_year, end_day):
 
 @app.route('/api/chart_data')
 def chart_data():
-    try:
-        # Get filter parameters from the query string
-        start_year = request.args.get('start_year')
-        end_year = request.args.get('end_year')
-        start_day = request.args.get('start_day')
-        end_day = request.args.get('end_day')
-        data_source = request.args.get('source')  # Get the selected data source
+    # Get filter parameters from the query string
+    start_year = int(request.args.get('start_year'))
+    end_year = int(request.args.get('end_year'))
+    start_day = int(request.args.get('start_day'))
+    end_day = int(request.args.get('end_day'))
+    data_source = request.args.get('source')  # Get the selected data source
 
-        # Check if any of the query parameters are missing or not numeric
-        if any(param is None or not param.isdigit() for param in [start_year, end_year, start_day, end_day]) or data_source is None:
-            return jsonify({'error': 'One or more query parameters are missing or invalid'}), 400
+    # Define the Excel file path based on the selected data source
+    file_path = f'static/{data_source}.xlsx'
 
-        # Convert parameters to integers
-        start_year = int(start_year)
-        end_year = int(end_year)
-        start_day = int(start_day)
-        end_day = int(end_day)
+    # Generate chart data
+    years, values = generate_chart_data(file_path, start_year, start_day, end_year, end_day)
 
-        # Define the Excel file path based on the selected data source
-        file_path = f'static/{data_source}.xlsx'
-
-        # Generate chart data
-        years, values = generate_chart_data(file_path, start_year, start_day, end_year, end_day)
-
-        if years is not None and values is not None:
-            return jsonify({'years': years, 'values': values})
-        else:
-            return jsonify({'error': 'No data found for the specified range'}), 404
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    if years is not None and values is not None:
+        return jsonify({'years': years, 'values': values})
+    else:
+        return jsonify({'error': 'No data found for the specified range'}), 404
 
 
 
